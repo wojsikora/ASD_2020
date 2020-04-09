5a3ee99a06cf0ce58e73ae842be2a2bbec2d74d7
dimamik <57190429+dimamik@users.noreply.github.com>
2020-04-09 22:53:47 +0200

    main

---
+++ Sorting arrays/Geekforgeeks_problems/zad_1_sort_by_frequensy.py
@@ -0,0 +1,14 @@
+""" 
+Print the elements of an array in the decreasing frequency if 2 numbers have same frequency then print the one which came first.
+
+Input:  arr[] = {2, 5, 2, 8, 5, 6, 8, 8}
+Output: arr[] = {8, 8, 8, 2, 2, 5, 5, 6}
+ """
+ 
+
+
+""" 
+1. n^2 naive solution
+
+2. 
+ """
