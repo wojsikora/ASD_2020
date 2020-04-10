fb73f0da2c332f0e7657fb6a6205e0075c3de387
dimamik <57190429+dimamik@users.noreply.github.com>
2020-04-10 19:00:23 +0200

    main

--- Sorting arrays/Geekforgeeks_problems/Zad_2_Count_Inversions.py
+++ Sorting arrays/Geekforgeeks_problems/Zad_2_Count_Inversions.py
@@ -18,6 +18,50 @@ Explanation: Given array has six inversions:
     A = [1,3,5,7]     B = [2,4,6,8]
     For each step of merge, if A[i]>B[j] then:
         a) count_inv+=(mid-i+1), because all elements from A with index >i would have inversion with B[j] and adding 1 because Merge sorts takes middle into arr A and we need to include this element itself
-        a
+        
 
  """
+
+def merge(tab,p,sr,k):
+    # Uwaga, sr is included in left_tab
+    counter=0
+    i=p
+    j=sr+1
+    tab_to_ret = [0] * (k-p+1)
+    curr_index=0
+    while i<=sr and j<=k:
+        if tab[i]<=tab[j]:
+            tab_to_ret[curr_index] = tab[i]
+            curr_index+=1
+            i+=1
+        else:
+            counter+=(sr-i+1)
+            tab_to_ret[curr_index] = tab[j]
+            curr_index+=1
+            j+=1
+    while i>sr and j<=k:
+        tab_to_ret[curr_index] = tab[j]
+        curr_index+=1
+        j+=1
+    while i<=sr and j>k:
+        tab_to_ret[curr_index] = tab[i]
+        curr_index+=1
+        i+=1
+    return tab_to_ret,counter
+counter=0
+def merge_sort(tab,p,k):
+    global counter
+    if p!=k:
+        sr=(p+k)//2
+        merge_sort(tab,p,sr)
+        merge_sort(tab,sr+1,k)
+        tab,counter_tmp = merge(tab,p,sr,k)
+        counter+=counter_tmp
+    return tab
+
+def zad_2(tab):
+    merge_sort(tab,0,len(tab)-1)
+    print(counter)
+
+zad_2([3, 1, 2])
+
