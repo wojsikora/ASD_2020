c6c66a684634a11645e068f42d1e63b7a6e454a4
dimamik <57190429+dimamik@users.noreply.github.com>
2020-04-11 17:56:30 +0200

    Rebuilding the heap structure

--- Sorting arrays/Heaps/heaps.py
+++ Sorting arrays/Heaps/heaps.py
@@ -1,18 +1,71 @@
-#Zdefiniujemy drzewo i jego parametry
-# tab[0] ma zawierać rozmiar
-# ZACZYNAMY OD 1!!
-def parent(i):return i//2
-def left(i): return i*2
-def right(i): return i*2 + 1
-def size(k): return k[0]
-#Naprawia kopiec działając w dół!
-#Функция создана для поднятия наибольшего значения в ветке (L,R,Korzeń) 
-#и восстановлению ущерба, причиненного поднятием (то есть опустил меньший элемент ниже 
-# и проверил условие MaxHeap)
-def heapify_max(k,i):   
-    
+#Defenitions for heap from 0 withoit size in arr[0]
+def parent(i):return (i-1)//2
+def left(i): return (i*2)+1
+def right(i): return (i*2) + 2
+
+def heapify_max_from0(tab,i):
+    """ 
+    Working with heap from 0 index
+     """
     L=left(i)
     R=right(i)
+    max=i
+    #Size is showing the last index
+    size=len(tab)-1
+    if L<=size and tab[L]>tab[max]:
+        max=L
+    if R<=size and tab[R]>tab[max]:
+        max=R
+    if max!=i:
+        #In this place should be struct switch if working with structs
+        tab[i],tab[max]=tab[max],tab[i]
+        heapify_max_from0(tab,max)
+    return tab
+
+def heapify_min_from0(tab,i):
+    """ 
+    Working with heap from 0 index
+     """
+    L=left(i)
+    R=right(i)
+    min=i
+    #Size is showing the last index
+    size=len(tab)-1
+    if L<=size and tab[L]<tab[min]:
+        min=L
+    if R<=size and tab[R]<tab[min]:
+        min=R
+    if min!=i:
+        #In this place should be struct switch if working with structs
+        tab[i],tab[min]=tab[min],tab[i]
+        heapify_min_from0(tab,min)
+    return tab
+#Build heap (change heapify to needed)
+def BuildHeap_from_0(tab):
+    """     
+    Zwykła tablica bez size w tab[0]
+    Funckja dziala z dołu w górę! (Bo musisz naprawic calkiem kopiec, a naprawiajac z dzieci
+nie robisz duzo razy to same) 
+"""
+    n=len(tab)-1
+    for i in range((n//2)-1,-1,-1):
+        heapify_max_from0(tab,i)
+    return tab
+
+
+#Definitions with heap from 1
+def parent_from_1(i):return i//2
+def left_from_1(i): return i*2
+def right_from_1(i): return i*2 + 1
+def size(k): return k[0]
+
+def heapify_max_from1(k,i):   
+    """ Naprawia kopiec działając w dół!
+    Функция создана для поднятия наибольшего значения в ветке (L,R,Korzeń) 
+    и восстановлению ущерба, причиненного поднятием (то есть опустил меньший элемент ниже 
+     и проверил условие MaxHeap) """
+    L=left_from_1(i)
+    R=right_from_1(i)
     max=i
     size=k[0]
     if L<=size and k[L]>k[max]:
@@ -21,40 +74,35 @@ def heapify_max(k,i):
         max=R
     if max!=i:
         k[i],k[max]=k[max],k[i]
-        heapify_max(k,max)
-def heapify_min(k,i):   
+        heapify_max_from1(k,max)
+
+def BuildHeap_from_1(tab):
+    """ 
+tab[0] ma zawierac size,
+Funckja dziala z dołu w górę! (Bo musisz naprawic calkiem kopiec, a naprawiajac z dzieci
+nie robisz duzo razy to same)
+     """
+    for i in range(tab[0]//2,0,-1):
+        heapify_max_from1(tab,i)
+    return tab
     
-    L=left(i)
-    R=right(i)
-    min=i
-    size=k[0]
-    if L<=size and k[L]<k[min]:
-        min=L
-    if R<=size and k[R]<k[min]:
-        min=R
-    if min!=i:
-        k[i],k[min]=k[min],k[i]
-        heapify_min(k,min)
-def BuildHeap_max(tab):
-    #Funckja dziala z dołu w górę! (Bo musisz naprawic calkiem kopiec, a naprawiajac z dzieci
-    #nie robisz duzo razy to same)
-    for i in range(tab[0]//2,0,-1):
-        heapify_max(tab,i)
-    return tab
-def BuildHeap_min(tab):
-    #Funckja dziala z dołu w górę! (Bo musisz naprawic calkiem kopiec, a naprawiajac z dzieci
-    #nie robisz duzo razy to same)
-    for i in range(tab[0]//2,0,-1):
-        heapify_min(tab,i)
-    return tab
-def heapsort(tab):
-    #Musimy dolaczyc na poczatek k[0] bo powyzsze funkcje biora to wartosc
+def Heapsort(tab):
+    """ 
+    !!HEAPSORT GAREK!!
+    Takes usual tab as an input and addes size as a tab[0] in O(n) time
+     """
     tab = [len(tab)] + tab
     #Budujemy heap
-    tab=BuildHeap_max(tab)
+    tab=BuildHeap_from_1(tab)
     for _ in range(tab[0],0,-1): 
-        #Czyli robimy posortowana tablice od konca przez zmniejszenie range of heapify_max i swap
+        #Czyli robimy posortowana tablice od konca przez zmniejszenie range of heapify i swap
         tab[tab[0]],tab[1]=tab[1],tab[tab[0]] #Last now is sorted max
-        tab[0]-=1 #Taking into the heapify_max 1 el less
-        heapify_max(tab,1)
+        tab[0]-=1 #Taking into the heapify 1 el less
+        heapify_max_from1(tab,1)
     return tab[1:]
+
+print(Heapsort([5,1,2,7,13,0,15]))
+
+
+
+    
