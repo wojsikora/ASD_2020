dc3f94adef0d836e7723941cbf5d66cf795e6a56
wojsikora <63751217+wojsikora@users.noreply.github.com>
2020-04-15 22:54:35 +0200

    Update Zad_1_7b.py

--- CWICZENIA/08.04_Stack_Queue_Grafs/Online/Zad_1_7b.py
+++ CWICZENIA/08.04_Stack_Queue_Grafs/Online/Zad_1_7b.py
@@ -1,7 +1,7 @@
 """ Kapitan  pewnego  statku zastanawia się, czy może wpłynąć do portu mimo, że nastąpił odpływ. Do dyspozycji ma mapę zatoki w postaci  tablicy M,  gdzie M[y][x]  to  głebokość  zatoki  na  pozycji  (x, y).  Jeśli  jest  ona  większa  niż  pewna wartość int T to statek może się tam znaleźć. Początkowo statek jest na pozycji (0,0) a port znajduje się na pozycji (n−1, m−1). Z danej pozycji statek może przepłynąć bezpośrednio jedynie na pozycję bezpośrednio obok  (to  znaczy,  na  pozycję,  której  dokładnie  jedna  ze  współrzędnych  różni  się  o  jeden).  Proszę  napisać funkcję rozwiązującą problem kapitana. """
 """ 
 Algorytm polega na:
-0) Sprawdzam czy B.val>=T (Bo nie moge stac na nizkiej wodzie a na B mam obowiązkowe być)
+0) Sprawdzam czy B.val>=T (Bo nie moge stac na niskiej wodzie a na B mam być obowiązkowo)
 BFS przejscie po macierzy biorąc pod uwagę int T, czyli:
 1) Wkładam na stos punkt A, jego dzieci (czyli takie punkty w ktorych moge sie znalezc), dzieci jego dzieci i td, dopoki nie przejde caly graf/nie bede mial dzieci lub napotkam sie na punkt B
 
