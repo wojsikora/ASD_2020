5b99d68209f556a59fe365c51cee9cc2ed931bed
wojsikora <63751217+wojsikora@users.noreply.github.com>
2020-04-15 22:51:27 +0200

    Update Zad_1.py

--- Bit_Algo/31.03_Zachłanne/Zad_1.py
+++ Bit_Algo/31.03_Zachłanne/Zad_1.py
@@ -1,6 +1,6 @@
 """ Tankowanie auta:W problemie tankowania paliwa nasz pojazd musi przemieścić się z punktu 0 do punktu F, a po drodze ma 
 stacje do tankowania paliwa si, przy czym 0 < s1 < s2 < ... < sn < F. Każda stacja jest identyfikowana przez jej 
 odległość od punktu 0, tzn. si to odległość pomiędzy i-tą stacją a puntem 0. Pojazd potrafi przejechać 
-odległość d bez potrzeby tankowania. """
+odległość d bez  tankowania. """
 test
 
